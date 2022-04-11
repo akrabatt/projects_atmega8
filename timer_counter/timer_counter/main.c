8cabc9b8e1784d6bc99f4219a69e0fc48e5b87b3
akrabatt <Starwars-1999@yandex.ru>
2022-04-11 21:54:06 +0200

    начинаем изучать таймеры

---
+++ timer_counter/timer_counter/main.c
@@ -0,0 +1,18 @@
+/*
+ * timer_counter.c
+ *
+ * Created: 11.04.2022 21:36:17
+ * Author : Дмитрий
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
