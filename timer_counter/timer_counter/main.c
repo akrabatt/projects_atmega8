70241b16b1e6c157704328cffe4c97c854e8c339
akrabatt <Starwars-1999@yandex.ru>
2022-04-12 21:49:53 +0200

    продолжаем осваивать таймер

--- timer_counter/timer_counter/main.c
+++ timer_counter/timer_counter/main.c
@@ -1,18 +1,11 @@
-/*
- * timer_counter.c
- *
- * Created: 11.04.2022 21:36:17
- * Author : Дмитрий
- */ 
-
 #include <avr/io.h>
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	DDRD = 0b11111111;
+	PORTD = 0b00000000;
+	
+
 }
 
