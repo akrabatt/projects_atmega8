4cd99c3548c43fededeefbe59e6be98f76fb8fec
akrabatt <Starwars-1999@yandex.ru>
2022-04-18 22:02:35 +0200

    продолжаем осваивать 16битный счетчик

--- 1_2_counter/1_2_counter/main.c
+++ 1_2_counter/1_2_counter/main.c
@@ -4,13 +4,22 @@
 
 int main(void)
 {
+	DDRD = 0b11111111;
+	PORTD = 0b00000000;
 	
-	TCCR1B = (CS10 << 0) | (CS11 << 1);
-	
+	//TCCR1B = (CS10 << 0) | (CS11 << 1);
+	TCCR1B = 0b00000011;
+	TCNT1 = 0; // обнуляем счетчик
 	
     while (1) 
     {
-		
+		if (TCNT1 == 0)
+		{
+			PORTD = 0b00001111;
+		}
+		if (TCNT1 == 0xFFFF/2)
+		{
+			PORTD = 0b11110000;
+		}
     }
 }
-
